b3415b22fb37c99ca5e69e4028edba19984b2eaf
Liam Marshall <liam@cpan.org>
2015-05-18 17:40:46 -0500

    Add scss task to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
+let sass = require('gulp-sass');
+
+gulp.task('scss', function () {
+  return gulp.src('./styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
 
 gulp.task('run', function () {
   return run("node_modules/.bin/electron .").exec();
