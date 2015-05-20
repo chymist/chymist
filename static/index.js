92570173c513d4335cbe0a4e7b218566a244eb3d
Liam Marshall <liam@cpan.org>
2015-05-19 19:42:09 -0500

    Atom-inspired index.js

---
+++ static/index.js
@@ -0,0 +1,13 @@
+window.onload = {
+  try {
+    /* foo */
+  } catch (e) {
+    var currentWindow = require('remote').getCurrentWindow();
+    currentWindow.setSize(800, 600);
+    currentWindow.center();
+    currentWindow.show();
+    currentWindow.openDevTools();
+
+    console.error(e.stack || e);
+  }
+};
