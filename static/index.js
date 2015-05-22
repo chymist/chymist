f6db28cdce6f6ba6e8961f6063bd4ec74b28dbfb
Liam Marshall <liam@cpan.org>
2015-05-21 20:51:01 -0500

    :art::bug: Fixup static/index.js to actually *do* stuff.

--- static/index.js
+++ static/index.js
@@ -1,6 +1,21 @@
 window.onload = function () {
   try {
-    /* foo */
+    var startTime = Date.now();
+    // parse url-encoded loadSettings. skip first 14 chars, they're just `?loadSettings=`.
+    var loadSettings = JSON.parse(decodeURIComponent(location.search.substr(14)))
+
+    if (loadSettings.devMode) {
+      require('coffee-script').register();
+    }
+
+    if (!loadSettings.devMode) {
+      require('coffee-script').register();
+    }
+
+    window.loadSettings = loadSettings;
+
+    require(loadSettings.bootstrapScript);
+    require('ipc').sendChannel('window-command', 'window:loaded');
   } catch (e) {
     var currentWindow = require('remote').getCurrentWindow();
     currentWindow.setSize(800, 600);
