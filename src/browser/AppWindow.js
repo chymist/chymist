c304f45b347c702df0df2910b727de1bc0f5ab08
Liam Marshall <liam@cpan.org>
2015-11-25 23:31:08 -0600

    Massive commit which creates a runnable app

---
+++ src/browser/AppWindow.js
@@ -0,0 +1,39 @@
+import {EventEmitter} from 'events';
+import BrowserWindow from 'browser-window';
+import path from 'path';
+import url from 'url';
+import _ from 'lodash';
+const debug = require('debug')('browser:AppWindow');
+
+export default class AppWindow extends EventEmitter {
+  constructor(options) {
+    super();
+    this.loadSettings = {
+      bootstrapScript: require.resolve('../renderer/main'),
+    };
+    _.extend(this.loadSettings, options);
+    const windowOpts = {
+      'web-preferences': {
+        'subpixel-font-scaling': true,
+        'direct-write': true,
+      },
+    };
+    _.extend(windowOpts, this.loadSettings);
+    debug(windowOpts);
+    this.window = new BrowserWindow(windowOpts);
+  }
+
+  show() {
+    const targetPath = path.join(__dirname, '..', '..', 'static', 'index.html');
+    const targetUrl = url.format({
+      protocol: 'file',
+      pathname: targetPath,
+      slashes: true,
+      query: {
+        loadSettings: JSON.stringify(this.loadSettings),
+      },
+    });
+    this.window.loadURL(targetUrl);
+    this.window.show();
+  }
+}
--- src/browser/Application.js
+++ src/browser/Application.js
@@ -1,9 +1,61 @@
 import {EventEmitter} from 'events';
+import AppWindow from './AppWindow';
+import path from 'path';
+const debug = require('debug')('browser:Application');
 
 export default class Application extends EventEmitter {
   constructor(options = {}) {
     super();
+    this.pkgJson = require(path.join(__dirname, '..', '..', 'package.json'));
+    this.windows = [];
+    this.options = options;
   }
 
-  open() {}
+  /**
+   * Opens up a new application window
+   */
+  open() {
+    const newWindow = this.createNewWindow(this.options);
+    this.addWindow(newWindow);
+    newWindow.show();
+    newWindow.on('closed', () => {
+      this.removeWindow(newWindow);
+    });
+  }
+
+  /**
+   * Performs the raw window creation logic
+   */
+  createNewWindow(options) {
+    const iconPath = path.join(__dirname, '..', '..', 'resources', 'icon.png');
+    const appWindow = new AppWindow({
+      title: this.pkgJson.productName,
+      icon: iconPath,
+      width: 1024,
+      height: 700,
+    });
+    return appWindow;
+  }
+
+  /**
+   * Add a window to the the array of windows.
+   *
+   * @param {AppWindow} appWindow The window to add to the array
+   */
+  addWindow(appWindow) {
+    this.windows.push(appWindow);
+  }
+
+  /**
+   * Remove a window from the array of windows.
+   *
+   * @param {AppWindow} appWindow The window to remove
+   */
+  removeWindow(appWindow) {
+    this.windows.forEach((win, index) => {
+      if (win === appWindow) {
+        this.windows.splice(index, 1);
+      }
+    });
+  }
 }
