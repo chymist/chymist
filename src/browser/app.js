0806c5e907a1c411b66d609ae466331a5c2cb704
Liam Marshall <liam@cpan.org>
2015-11-25 23:30:17 -0600

    Dump errors to stdout

--- src/browser/app.js
+++ src/browser/app.js
@@ -2,6 +2,21 @@ const argv = require('minimist')(process.argv.slice(2));
 const debug = require('debug')('browser:app');
 const shellStartTime = Date.now();
 
+// make errors print to terminal
+process.on('uncaughtException', function printUncaughtException(error) {
+  if (error === null) {
+    const error = {};
+  }
+
+  if (error.message !== null) {
+    console.error(error.message);
+  }
+
+  if (error.stack !== null) {
+    console.error(error.stack);
+  }
+});
+
 import Application from './Application';
 
 const application = new Application(argv);
